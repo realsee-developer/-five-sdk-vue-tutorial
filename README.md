2bc0962642dc9e45eb15de55e61f483fe60ed11f
Junchao <junchaocode@gmail.com>
2022-11-07 14:25:16 +0800

    add docs

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# -five-sdk-vue-tutorial
+# Five SDK Tutorial
+> Javascript with vue
+
+Five SDK [**Javascript with vue**  教程示例](https://open-platform.realsee.com/developer/docs/front/3d-space/handbook/vue/get-started/)
+
+## 系统要求
+
+- node >= 12.x
+- 一个现代浏览器: 
+
+| Safari | Safari on iOS | Chrome | Chrome for Android | Edge  | Firefox |
+| :----: | :-----------: | -----: | :----------------: | :---: | :-----: |
+|  >= 9  |     >= 9      |  >= 49 |       >= 93        | >= 13 |  >= 45  |
+
+## 安装依赖
+
+```shell
+npm install
+```
+
+## 本地运行
+
+```shell
+npm run dev
+```
---
+++ src/0.getting-started/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <FiveProvider :work="work">
+        <FiveCanvas :width="width" :height="height" />
+    </FiveProvider>
+</template>
+
+<script setup>
+
+import { FiveProvider, FiveCanvas } from "@realsee/five/vue";
+import { parseWork } from "@realsee/five";
+import { ref } from "vue";
+import { useWindowDimensions } from "./useWindowDimensions";
+
+const work = ref();
+const workURL = "https://vrlab-public.ljcdn.com/release/static/image/release/five/work-sample/07bdc58f413bc5494f05c7cbb5cbdce4/work.json";
+
+fetch(workURL).then(response => response.text()).then(text => work.value = parseWork(text));
+const { width, height } = useWindowDimensions();
+</script>
